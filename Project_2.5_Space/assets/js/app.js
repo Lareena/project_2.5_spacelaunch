ef8f4b5a76dd192ffb3f157987b7093c4633fec0
Lareena <lareena.e.llamado@gmail.com>
2021-01-16 10:44:32 -0500

    update app.js

--- Project_2.5_Space/assets/js/app.js
+++ Project_2.5_Space/assets/js/app.js
@@ -8,31 +8,31 @@ async function chart() {
 
 chart()
 
-// d3.json("launches.json").then((data) => {
-//   console.log("hello world")
-  //console.log(data)
-  //Create trace
-  // var trace1 = {
-  //     x: data.Country,
-  //     y: data.StatusMission,
-  //     type: "bar",
-  //     name: "Successful Launches"
-  // };
+d3.json("launches.json").then((data) => {
+  console.log("hello world")
+  console.log(data)
+  Create trace
+  var trace1 = {
+      x: data.Country,
+      y: data.StatusMission,
+      type: "bar",
+      name: "Successful Launches"
+  };
   
-  // //create data array for plot
-  // var data = [trace1];
+  //create data array for plot
+  var data = [trace1];
 
-  // //define plot layout
-  // var layout = {
-  //     title: "successful launches per country",
-  //     xaxis: { title: "Country" },
-  //     yaxis: { title: "success" }
+  //define plot layout
+  var layout = {
+      title: "successful launches per country",
+      xaxis: { title: "Country" },
+      yaxis: { title: "success" }
      
-  // };
+  };
 
-  // //plot chart to div tage with id "plot"
-  // Plotly.newPlot("plot", data, layout);
-// });
+  //plot chart to div tage with id "plot"
+  Plotly.newPlot("plot", data, layout);
+});
 
 
 
