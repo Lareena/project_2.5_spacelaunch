4c2daa9d89a62c202317a5e3cedbe3b89c16612b
Lareena <lareena.e.llamado@gmail.com>
2021-01-16 09:51:45 -0500

    updated app.py

--- Project_2.5_Space/assets/js/app.js
+++ Project_2.5_Space/assets/js/app.js
@@ -1,27 +1,38 @@
 var CountryObj = [];
 var StatusMissionObj = [];
 
+async function chart() {
+  const dataset = await d3.json("./launches.json")
+  console.log(dataset)
+}
 
-d3.json("launches.json").then((data) => {
+chart()
+
+// d3.json("launches.json").then((data) => {
+//   console.log("hello world")
+  //console.log(data)
   //Create trace
-  var trace1 = {
-      x: data.Country,
-      y: data.StatusMission,
-      type: "bar",
-      name: "Successful Launches"
-  };
+  // var trace1 = {
+  //     x: data.Country,
+  //     y: data.StatusMission,
+  //     type: "bar",
+  //     name: "Successful Launches"
+  // };
   
-  //create data array for plot
-  var data = [trace1];
+  // //create data array for plot
+  // var data = [trace1];
 
-  //define plot layout
-  var layout = {
-      title: "successful launches per country"
-      xaxis: { title: "Country" },
-      yaxis: { title: "success" }
+  // //define plot layout
+  // var layout = {
+  //     title: "successful launches per country",
+  //     xaxis: { title: "Country" },
+  //     yaxis: { title: "success" }
      
-  };
+  // };
 
-  //plot chart to div tage with id "plot"
-  Plotly.newPlot("plot", data, layout);
-});
+  // //plot chart to div tage with id "plot"
+  // Plotly.newPlot("plot", data, layout);
+// });
+
+
+
