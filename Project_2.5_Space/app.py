3bc4052108b337b5dfe878c1bdfa66fa22a698a7
Scotty P <provencher.scottucf@gmail.com>
2021-01-18 23:20:53 -0500

    removed unnecessary files

--- Project_2.5_Space/app.py
+++ Project_2.5_Space/app.py
@@ -57,7 +57,7 @@ def names():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of all passenger names"""
+    """Return a list of all Launch data names"""
     # Query all passengers
     results = session.execute(session.query(Space_Corrected)).fetchall()
 
@@ -173,28 +173,7 @@ def company():
     # return df.to_json(orient="records")
 
 
-# from homework, delete before submit!!!!
-# @app.route("/api/v1.0/passengers")
-# def passengers():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
 
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
-#     session.close()
-
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_passengers = []
-#     for name, age, sex in results:
-#         passenger_dict = {}
-#         passenger_dict["name"] = name
-#         passenger_dict["age"] = age
-#         passenger_dict["sex"] = sex
-#         all_passengers.append(passenger_dict)
-
-    # return jsonify(all_passengers)
 
 
 if __name__ == '__main__':
--- Project_2.5_Space/static/js/app.js
+++ Project_2.5_Space/static/js/app.js
@@ -108,37 +108,4 @@ init()
 
 
 
-// //data to be used for the plot
-
-  // var chartData = [trace2];
-
-
-
-// d3.json("launches.json").then((data) => {
-//   console.log("hello world")
-  //console.log(data)
-  //Create trace
-  // var trace1 = {
-  //     x: data.Country,
-  //     y: data.StatusMission,
-  //     type: "bar",
-  //     name: "Successful Launches"
-  // };
-  
-  // //create data array for plot
-  // var data = [trace1];
-
-  // //define plot layout
-  // var layout = {
-  //     title: "successful launches per country",
-  //     xaxis: { title: "Country" },
-  //     yaxis: { title: "success" }
-     
-  // };
-
-  // //plot chart to div tage with id "plot"
-  // Plotly.newPlot("plot", data, layout);
-// });
-
-
 
